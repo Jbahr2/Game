c3fca5b87be80ca1ee9c0c24c039c3ef2c32ab3c
Parker Spires <44887705+mrdrparker@users.noreply.github.com>
2020-03-03 16:19:03 -0700

    Update Tile.java

--- game/src/Tile.java
+++ game/src/Tile.java
@@ -1,13 +1,23 @@
+/* *
+ * author: Group H
+ * date: 3/3/2020
+ * 
+ * Tile class
+ * utilizes panels now instead of labels
+ * 
+ * */
+
 import java.awt.Color;
-import java.awt.Dimension;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Tile extends JLabel {
-	public Tile(String text) {
-		super(text);
-		setOpaque(true);
-		setBorder(BorderFactory.createEmptyBorder(16, 16, 16, 16));
-		setBackground(new Color(254, 211, 48));
-	}
+public class Tile extends JPanel {
+    public Tile() {
+        super();
+        setOpaque(true);
+        setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+        setPreferredSize(new Dimension(100, 100));
+        setBackground(new Color(254, 211, 48));
+    }
 }
