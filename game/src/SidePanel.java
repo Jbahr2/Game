59e2f0f376cb047a8ba4b8a9bc931466e9effaf6
Parker Spires <44887705+mrdrparker@users.noreply.github.com>
2020-03-03 16:18:38 -0700

    Update SidePanel.java

--- game/src/SidePanel.java
+++ game/src/SidePanel.java
@@ -1,4 +1,13 @@
+
+/* *
+ * @author: Group H
+ * date: 3/3/2020
+ * 
+ * Creates the tiles on the side of the game board.
+ *  
+ * */
 import java.awt.Color;
+
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -7,22 +16,29 @@ import java.awt.Insets;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class SidePanel extends JPanel {
 
-	public SidePanel() {
-		setAlignmentX(CENTER_ALIGNMENT);
+    public SidePanel(int side) {
+        setAlignmentX(CENTER_ALIGNMENT);
 
-		setBackground(new Color(0, 0, 0, 0));
+        setBackground(new Color(0, 0, 0, 0));
 
-		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        
+        /* initialize a tile counter */
+        int count = 0;
+        if(side == 1)
+            count = 8; /* set tile counter to 8 for right sidePanel */
 
-		// initialize tiles for each player
-		for (int i = 0; i < 8; i++) {
-			Tile tile = new Tile("R");
-			tile.setBackground(new Color(69, 170, 242));
-			add(tile);
-			add(Box.createRigidArea(new Dimension(8, 8)));
-		}
-	}
+        // initialize tiles for each player
+        for (int i = 0; i < 8; i++) {
+            Tile tile = new Tile();
+            tile.add(new JLabel(""+(count+i)));
+            tile.setBackground(new Color(69, 170, 242));
+            add(tile);
+            add(Box.createRigidArea(new Dimension(8, 8)));
+        }
+    }
 }
