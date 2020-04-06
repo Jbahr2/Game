33e1f8fd46f53c755f0e11969c8d941cb39aae10
booli <booli@DESKTOP-7JF3VP3>
2020-04-05 23:02:02 -0600

    Made gameboard able to handle any number of tiles (I got bored okay)

--- game/src/GameBoard.java
+++ game/src/GameBoard.java
@@ -15,19 +15,23 @@ import javax.swing.JPanel;
 public class GameBoard extends JPanel {
 	private TileWrapper[] tileWrapper;
     public GameBoard(ClickSwapper swapper, FileDecoder filedecoder) {
-    	int Gridnum = (int)Math.sqrt(filedecoder.getTileNum());
+    	int Gridnum = (int)Math.floor(Math.sqrt(filedecoder.getTileNum()));
         GridBagLayout gbl = new GridBagLayout();
         setLayout(gbl);
 
-        tileWrapper = new TileWrapper[(int)Math.pow(Gridnum, 2)];
+        tileWrapper = new TileWrapper[filedecoder.getTileNum()];
+        
+        //if tile count is not a square number it calculates the overflow and accounts for it else it returns 0
+        int tileoverflowY = (int)(Math.ceil((filedecoder.getTileNum() - Math.pow(Gridnum,2)) / Gridnum));
+        int tileoverflowX = ((int)(filedecoder.getTileNum() - Math.pow(Gridnum,2)) % Gridnum);
         
         GridBagConstraints center = new GridBagConstraints();
         setBackground(new Color(0, 0, 0, 0));
-
-        // initialize grid of 4x4 tiles
-        for (int y = 0; y < Gridnum; y++) {
+        // initialize grid of X by X tiles
+        for (int y = 0; y < (Gridnum + tileoverflowY); y++) {
             center.gridy = y;
-            for (int x = 0; x < Gridnum; x++) {
+            for (int x = 0; x < (Gridnum + (((y + 1 >= (Gridnum + tileoverflowY)) && (tileoverflowY > 0) && (tileoverflowX != 0)) ? (tileoverflowX - Gridnum) : 0)); x++) {
+            	System.out.println(x);
                 TileWrapper tileWrapper = new TileWrapper(swapper);
                 this.tileWrapper[y * Gridnum + x] = tileWrapper;
                 tileWrapper.InitializeTile(tileWrapper.getTile());
